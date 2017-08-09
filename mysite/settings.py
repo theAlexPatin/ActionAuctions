527361ff71ff57ddc8a3bb26b59e71c3281117e2
Patin <apatin@LAMU02T7ABXGTFM.uhc.com>
2017-08-09 10:22:33 -0400

    got static files working

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,7 +101,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
@@ -139,4 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+
+
+
+
+
+
+
