b5ffab4a568146276d96897d6dd3e7b57e171506
lionpathsux <ajp5755@psu.edu>
2017-11-09 19:08:29 -0500

    first node commit

--- mysite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('winner.urls')),
-    url(r'^', include('auctions.urls'))
-]
--- winner/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-from . import views
-
-urlpatterns = [
-    url(r'^winner/(?P<winner_id>[\w\-]+)/$', views.winner),
-    url(r'^winner/$', views.button_press)
-]
