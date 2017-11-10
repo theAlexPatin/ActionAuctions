972c7a2eddc7a9433c6c8b2f621be9826bb9cd87
lionpathsux <ajp5755@psu.edu>
2017-11-10 15:08:17 -0500

    added Dynamo integration for one page

--- routes/auctions.js
+++ routes/auctions.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+var modules = require('../app');
+var ddb = modules.ddb;
 
 /* GET home page. */
 router.get('/:auction_id', function(req, res, next) {
-	console.log(req.params);
-	res.render('auction', {auction_id: req.params.auction_id});
+	var params = {
+		TableName: "Auctions",
+		Key:{
+			"auction_id":req.params.auction_id
+		}
+	};
+	ddb.get(params, function(err, data){
+		if (err) {
+        	console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+	    } else {
+	        res.render('auction', data['Item']);
+	    }
+	});
 });
 
 module.exports = router;
