c8b124e7829a1313999745ddd257654fea88455d
lionpathsux <ajp5755@psu.edu>
2017-11-11 19:18:56 -0500

    finished checkout functionality

--- routes/charge.js
+++ routes/charge.js
@@ -52,54 +52,91 @@ router.post('/', function(req, res, next) {
 			  source: stripe_token,
 			  description: "Charge from Charity Labs"
 			}, function(err, charge) {
-				var stripe_id = charge['id'];
-				var date = dateFormat(new Date(),"isoDateTime");
+				if (err)
+					res.redirect('/payment-error');
+				else{
+					var stripe_id = charge['id'];
+					var date = dateFormat(new Date(),"isoDateTime");
 
-				/*CREATE BID ENTRY IN DATABASE*/
-				var params = {
-				    TableName:'Bids',
-				    Item:{
-						"stripe_id":stripe_id,
-						"auction_id":auction_id,
-						"amount": Math.floor(amount*100),
-						"email":email,
-						"time":date
-				    }
-				};
-				console.log(params);
-				ddb.put(params, function(err, data) {
-				    if (err) {
-				        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-				    } else {
-				        console.log("Added item:", JSON.stringify(data, null, 2));
-				    }
-				});
+					/*CREATE BID ENTRY IN DATABASE*/
+					var params = {
+					    TableName:'Bids',
+					    Item:{
+							"stripe_id":stripe_id,
+							"auction_id":auction_id,
+							"amount": Math.floor(amount*100),
+							"email":email,
+							"time":date
+					    }
+					};
+					console.log(params);
+					ddb.put(params, function(err, data) {
+					    if (err) {
+					        console.error("Unable to add item");
+					    } else {
+					        console.log("Added item");
+					    }
+					});
 
-		    	/*UPDATE AUCTION IF NECESSARY*/
+			    	/*UPDATE AUCTION DATABASE RECORD*/
+			    	var current_amount = data['current_amt'] + Math.floor(amount*100);
+			   		//If this bid is greater than previous highest, update that
+			    	if(Math.floor(amount*100) > data['highest_bid']){
+			    		var UpdateExpression = "set highest_bid=:h, highest_bidder=:b, current_amt=:c";
+					    var ExpressionAttributeValues = {
+					        ":c":current_amount,
+					        ":h":Math.floor(amount*100),
+					        ":b":stripe_id
+					    };
+			    	} else{
+			    		var UpdateExpression = "set current_amt=:c";
+					    var ExpressionAttributeValues = {
+					        ":c":current_amount
+					    };
+			    	}
+			    	console.log(ExpressionAttributeValues)
+			    	var params = {
+			    		TableName:"Auctions",
+			    		Key:{
+			    			"auction_id":auction_id
+			    		},
+			    		UpdateExpression: UpdateExpression,
+			    		ExpressionAttributeValues: ExpressionAttributeValues,
+			  			ReturnValues:"UPDATED_NEW"
+			    	}
+			    	ddb.update(params, function(err, data) {
+					    if (err) {
+					        console.error("Unable to update item.", JSON.stringify(err, null, 2));
+					    } else {
+					        console.log("UpdateItem succeeded");
+					    }
+					});
 
+			    	/*SEND EMAIL*/
+			    	var mailOptions = {
+					  from: service_email,
+					  to: email,
+					  subject: `Confirming your Donation to ${charity}`,
+					  text: `Hey, ${first_name}!\n\nThank you for your generous donation to ${charity}!\n\nMake sure to check back in on ${end_time} to see if you've won!\n\n      Donation Total: $${amount}\n\n\nSincerely,\nCharity Labs Team`
+					};
+					transporter.sendMail(mailOptions, function(error, info){
+					  if (error) {
+					    console.log(error);
+					  } else {
+					    console.log('Email sent: ' + info.response);
+					  }
+					});
 
-		    	/*SEND EMAIL*/
-		    	var mailOptions = {
-				  from: service_email,
-				  to: email,
-				  subject: `Confirming your Donation to ${charity}`,
-				  text: `Hey, ${first_name}!\n\nThank you for your generous donation to ${charity}!\n\nMake sure to check back in on ${end_time} to see if you've won!\n\n      Donation Total: $${amount}\n\n\nSincerely,\nCharity Labs Team`
-				};
-				transporter.sendMail(mailOptions, function(error, info){
-				  if (error) {
-				    console.log(error);
-				  } else {
-				    console.log('Email sent: ' + info.response);
-				  }
-				});
-
-				/*RENDER CONFIRMATION PAGE*/
-				context = data;
-				context['auction_link'] = req.headers.host+'/auction/'+data['auction_id'];
-				context['first_name'] = first_name;
-				context['amount'] = amount;
-				context['end_time'] = end_time;
-				res.render('confirmation', context);
+					/*RENDER CONFIRMATION PAGE*/
+					context = data;
+					context['auction_link'] = req.headers.host+'/auction/'+data['auction_id'];
+					context['first_name'] = first_name;
+					context['amount'] = amount;
+					context['end_time'] = end_time;
+					delete context['highest_bidder'];
+					delete context['highest_bid'];
+					res.render('confirmation', context);
+				}
 			});
 	    }
 	});
