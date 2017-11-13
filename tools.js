535d7927c23d5ba7101e36299828dc30a4711654
lionpathsux <ajp5755@psu.edu>
2017-11-13 18:01:19 -0500

    finished cron stuff

--- tools.js
+++ tools.js
@@ -45,89 +45,26 @@ module.exports = {
 	},
 
 	start_job: function(auction_id, ending_time){
-		var b = ending_time.split(/\D+/);
-		var date = new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
-		var job = schedule.scheduleJob(date,function(argument) {
-			console.log("notifying winner for "+auction_id);
-			notify_winner(auction_id);
-			job.cancel();
-		});
+		start_job(auction_id, ending_time);
 	},
 
-	restart_jobs: function(ddb){
+	restart_jobs: function(d){
+		ddb = d;
 		ddb.scan({TableName:"Auctions"},function(err,data){
 			if(err)
 				console.log('error scanning auctions');
 			else{
 				for (var i = 0; i < data['Items'].length; i++){
-					//only schedule if not ended
 					auction = data['Items'][i];
-					start_job(auction['auction_id'], auction['ending_time']);
+					if (!auction['has_ended'])
+						start_job(auction['auction_id'], auction['ending_time']);
 				}
 			}
 		});
 	},
 
 	notify_winner: function(auction_id){
-		var params = {
-			TableName: "Auctions",
-			Key:{
-				"auction_id":auction_id
-			}
-		};
-		ddb.get(params, function(err, data){
-			if (err)
-	        	console.log("Error fetching auction from database");
-		    else{
-		    	auction_data = data['Item'];
-		    	console.log('got auction data');
-		    	params = {
-		    		TableName: "Bids",
-		    		Key:{
-		    			"bidder_id":auction_data['highest_bidder']
-		    		}
-		    	}
-		    	ddb.get(params, function(err, data){
-		    		if(err){
-		    			console.log("Error fetching highest bidder");
-		    		} else{
-		    			console.log("got bidder data");
-		    			bidder_data = data['Item'];
-		    			var email  = bidder_data['email'];
-		    			var first_name = bidder_data['first_name'];
-		    			var amount = parseFloat(new String(Math.floor(auction_data['current_amt'] / 2)/100)).toFixed(2);
-				    	var total = parseFloat(new String(Math.floor(auction_data['current_amt'])/100)).toFixed(2);
-				    	var charity = auction_data['charity'];
-		    			var link = host + '/winner/' + bidder_data['bidder_id'];
-		    			console.log(link);
-					    var params = {
-				    		TableName:"Auctions",
-				    		Key:{
-				    			"auction_id":auction_id
-				    		},
-				    		UpdateExpression: "set winner_url=:w, has_ended=:h, reimbursed=:r",
-				    		ExpressionAttributeValues: {
-						        ":w":bidder_data['bidder_id'],
-						        ":r":false,
-						        ":h":true
-						    },
-				  			ReturnValues:"UPDATED_NEW"
-				    	}
-					    ddb.update(params, function(err, data) {
-						    if (err) {
-						        console.error("Unable to update item.", JSON.stringify(err, null, 2));
-						    } else {
-						    	console.log("updated auction data");
-				    			var subject=`Congrats! You've won auction ${auction_id}`;
-								var text = `Congratulations, ${first_name}!\n\nYou made the largest donation. All donations totaled to $${total}, so you've won $${amount}.\n\nBut wait!\n\nYou can still choose to donate your earnings and feel great about yourself!\n\nFollow the link below to redeem your earnings or donate them to ${charity}\n\n${link}\n\nSincreley,\nCharity Labs Team`;
-								sendEmail(subject, text, email);
-						    }
-						});
-			    			
-		    		}
-		    	});
-		    }
-		});
+		notify_winner(auction_id);
 	}
 };
 
@@ -136,12 +73,12 @@ function start_job(auction_id, ending_time){
 	var date = new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
 	var job = schedule.scheduleJob(date,function(argument) {
 		console.log("notifying winner for "+auction_id);
-		tools.notify_winner(auction_id);
+		notify_winner(auction_id);
 		job.cancel();
 	});
 }
 
-notify_winner: function(auction_id){
+function notify_winner(auction_id){
 	var params = {
 		TableName: "Auctions",
 		Key:{
@@ -163,6 +100,13 @@ notify_winner: function(auction_id){
 	    	ddb.get(params, function(err, data){
 	    		if(err){
 	    			console.log("Error fetching highest bidder");
+	    			params = {
+	    				TableName:'Auctions',
+	    				Key:{'auction_id':auction_id},
+	    				UpdateExpression:'set has_ended=:h',
+	    				ExpressionAttributeValues:{':h':true}
+	    			}
+	    			ddb.update(params, function(){})
 	    		} else{
 	    			console.log("got bidder data");
 	    			bidder_data = data['Item'];
