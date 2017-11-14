f11894386e6d867365522db89297e04f82c1406f
lionpathsux <ajp5755@psu.edu>
2017-11-14 18:03:16 -0500

    fixed reimbursement job issue for no donations

--- tools.js
+++ tools.js
@@ -94,59 +94,61 @@ function notify_winner(auction_id){
         	console.log("Error fetching auction from database");
 	    else{
 	    	auction_data = data['Item'];
-	    	console.log('got auction data');
-	    	params = {
-	    		TableName: "Bids",
-	    		Key:{
-	    			"bidder_id":auction_data['highest_bidder']
-	    		}
+	    	if ('winner_url' in auction_data){
+	    		console.log('got auction data');
+		    	params = {
+		    		TableName: "Bids",
+		    		Key:{
+		    			"bidder_id":auction_data['highest_bidder']
+		    		}
+		    	}
+		    	ddb.get(params, function(err, data){
+		    		if(err){
+		    			console.log("Error fetching highest bidder");
+		    			params = {
+		    				TableName:'Auctions',
+		    				Key:{'auction_id':auction_id},
+		    				UpdateExpression:'set has_ended=:h',
+		    				ExpressionAttributeValues:{':h':true}
+		    			}
+		    			ddb.update(params, function(){})
+		    		} else{
+		    			console.log("got bidder data");
+		    			bidder_data = data['Item'];
+		    			var email  = bidder_data['email'];
+		    			var first_name = bidder_data['first_name'];
+		    			var amount = parseFloat(new String(Math.floor(auction_data['current_amt'] / 2)/100)).toFixed(2);
+				    	var total = parseFloat(new String(Math.floor(auction_data['current_amt'])/100)).toFixed(2);
+				    	var charity = auction_data['charity'];
+		    			var link = host + '/winner/' + bidder_data['bidder_id'];
+		    			console.log(link);
+					    var params = {
+				    		TableName:"Auctions",
+				    		Key:{
+				    			"auction_id":auction_id
+				    		},
+				    		UpdateExpression: "set winner_url=:w, has_ended=:h, reimbursed=:r",
+				    		ExpressionAttributeValues: {
+						        ":w":bidder_data['bidder_id'],
+						        ":r":false,
+						        ":h":true
+						    },
+				  			ReturnValues:"UPDATED_NEW"
+				    	}
+					    ddb.update(params, function(err, data) {
+						    if (err) {
+						        console.error("Unable to update item.", JSON.stringify(err, null, 2));
+						    } else {
+						    	console.log("updated auction data");
+				    			var subject=`Congrats! You've won auction ${auction_id}`;
+								var text = `Congratulations, ${first_name}!\n\nYou made the largest donation. All donations totaled to $${total}, so you've won $${amount}.\n\nBut wait!\n\nYou can still choose to donate your earnings and feel great about yourself!\n\nFollow the link below to redeem your earnings or donate them to ${charity}\n\n${link}\n\nSincreley,\nCharity Labs Team`;
+								sendEmail(subject, text, email);
+						    }
+						});
+			    			
+		    		}
+		    	});
 	    	}
-	    	ddb.get(params, function(err, data){
-	    		if(err){
-	    			console.log("Error fetching highest bidder");
-	    			params = {
-	    				TableName:'Auctions',
-	    				Key:{'auction_id':auction_id},
-	    				UpdateExpression:'set has_ended=:h',
-	    				ExpressionAttributeValues:{':h':true}
-	    			}
-	    			ddb.update(params, function(){})
-	    		} else{
-	    			console.log("got bidder data");
-	    			bidder_data = data['Item'];
-	    			var email  = bidder_data['email'];
-	    			var first_name = bidder_data['first_name'];
-	    			var amount = parseFloat(new String(Math.floor(auction_data['current_amt'] / 2)/100)).toFixed(2);
-			    	var total = parseFloat(new String(Math.floor(auction_data['current_amt'])/100)).toFixed(2);
-			    	var charity = auction_data['charity'];
-	    			var link = host + '/winner/' + bidder_data['bidder_id'];
-	    			console.log(link);
-				    var params = {
-			    		TableName:"Auctions",
-			    		Key:{
-			    			"auction_id":auction_id
-			    		},
-			    		UpdateExpression: "set winner_url=:w, has_ended=:h, reimbursed=:r",
-			    		ExpressionAttributeValues: {
-					        ":w":bidder_data['bidder_id'],
-					        ":r":false,
-					        ":h":true
-					    },
-			  			ReturnValues:"UPDATED_NEW"
-			    	}
-				    ddb.update(params, function(err, data) {
-					    if (err) {
-					        console.error("Unable to update item.", JSON.stringify(err, null, 2));
-					    } else {
-					    	console.log("updated auction data");
-			    			var subject=`Congrats! You've won auction ${auction_id}`;
-							var text = `Congratulations, ${first_name}!\n\nYou made the largest donation. All donations totaled to $${total}, so you've won $${amount}.\n\nBut wait!\n\nYou can still choose to donate your earnings and feel great about yourself!\n\nFollow the link below to redeem your earnings or donate them to ${charity}\n\n${link}\n\nSincreley,\nCharity Labs Team`;
-							sendEmail(subject, text, email);
-					    }
-					});
-		    			
-	    		}
-	    	});
 	    }
 	});
 }
