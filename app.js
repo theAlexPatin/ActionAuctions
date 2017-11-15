17dcc7adced08c1e1f4efae9f421565845081cee
lionpathsux <ajp5755@psu.edu>
2017-11-15 12:20:43 -0500

    added helmet

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var helmet = require('helmet');
 
 var AWS = require('aws-sdk');
 var settings = require('./environment');
@@ -40,7 +41,7 @@ var admin = require('./routes/admin');
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(helmet());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
