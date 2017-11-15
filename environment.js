fcac16d2e78bc35a3bbf9a3dbe0653de51cd516e
lionpathsux <ajp5755@psu.edu>
2017-11-15 01:18:17 -0500

    environment

---
+++ environment.js
@@ -0,0 +1,8 @@
+module.exports = {
+	STRIPE_API_KEY:"sk_test_5touzY5sFtfwT0lBuwbvD4l6", 
+	STRIPE_PUBLIC_KEY:"pk_test_08b8DgWK3wRVmfdrhCtg3mVa",
+	STRIPE_CLIENT_ID:'ca_BI52uK0mUKaVbtZnW1rfg7LKfvgGoWFY',
+	service_email:"admin@actionauctions.org",
+	email_pass:"Fuck Poverty123!",
+	base_url:"http://aaprod.us-east-1.elasticbeanstalk.com"
+}
